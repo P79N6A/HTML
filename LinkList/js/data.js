e30a4b608891b441ff3a853a078d98a063483549
Mario <myhamidi@hotmail.de>
2018-05-28 23:48:23 +0200

    Simple List of Links V1.0

---
+++ LinkList/js/data.js
@@ -0,0 +1,24 @@
+var var_data = {
+
+    "Start": 
+    [
+        {
+            "url": "https://www.facebook.com/", "img": "",
+        },
+        {
+            "url": "https://github.com/"
+        },
+
+    ],
+
+    "Next": 
+    [
+        {
+            "url": "https://www.amazon.com/",
+        },
+        {
+            "url": "http://www.netflix.com/WiHome", 
+        },
+    ],
+
+}
---
+++ LinkList/js/linklist.js
@@ -0,0 +1,36 @@
+
+//Var
+var mains = document.getElementsByClassName("main");
+
+for (j = 0; j < mains.length; j++) {
+    
+    //Var
+    var d = document.createElement("div");
+    var h = document.createElement("h6");
+    var current = document.getElementById(mains[j].id);
+    
+    //Manipulate
+    d.setAttribute("class", "mohi-line");
+    h.innerHTML = current.id;
+    
+    //Append
+    current.appendChild(h);
+    current.appendChild(d);
+
+    //For each antry
+    for (i = 0; i < var_data[current.id].length; i++) {
+        
+        //Var
+        var a = document.createElement("a");
+        var b = document.createElement("br");
+        
+        //Manipulate
+        a.setAttribute("href", var_data[current.id][i].url);
+        a.innerHTML = var_data[current.id][i].url;
+
+        //Append
+        current.appendChild(a);
+        current.appendChild(b);
+    };
+}
+
