fdae9531a47fee358f0942f2d7223796c5a46580
Mario <myhamidi@hotmail.de>
2018-06-12 23:45:09 +0200

    HTMl List V1.5
    Index:
    - BS optionally
    - Removed link to testcode.js
    
    Searchfilter.js:
    + Added Search to table rows
    
    linklist.css:
    +Added stylse to body and tables

--- 03 Linked List/js/searchfilter.js
+++ 03 Linked List/js/searchfilter.js
@@ -6,8 +6,9 @@ function mySearchfilter() {
 
     //Loop through all main divs:
     for (j = 0; j < mains.length; j++) {
-        var a_list = mains[j].getElementsByTagName("a");
+
         //Loop through all list items
+        var a_list = mains[j].getElementsByTagName("a");
         for (i = 0; i < a_list.length; i++) {
             a = a_list[i];
             if (a) {
@@ -15,6 +16,19 @@ function mySearchfilter() {
                     a.style.display = "";
                 } else {
                     a.style.display = "none";
+                }
+            }
+        }
+
+        //Loop through all table row items
+        var tr_list = mains[j].getElementsByTagName("tr");
+        for (i = 0; i < tr_list.length; i++) {
+            tr = tr_list[i];
+            if (tr) {
+                if (tr.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                    tr.style.display = "";
+                } else {
+                    tr.style.display = "none";
                 }
             }
         }
