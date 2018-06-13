f356a18fbcef64a6519f1494adefeff5fddabf2c
Mario <myhamidi@hotmail.de>
2018-06-13 23:54:53 +0200

    Data Page V1.6
    Refinement:
    o Rename to "Data Page"
    o style based on body.css and grid.css
    o simplified index.html

--- 03 Data Page/js/searchfilter.js
+++ 03 Data Page/js/searchfilter.js
@@ -1,14 +1,14 @@
 function mySearchfilter() {
     //Var
-    var mains = document.getElementsByClassName("main");
+    var seach_here = document.getElementsByClassName("seach-here");
     var input = document.getElementById("myInput");
     var filter = input.value.toUpperCase();
 
     //Loop through all main divs:
-    for (j = 0; j < mains.length; j++) {
+    for (j = 0; j < seach_here.length; j++) {
 
         //Loop through all list items
-        var a_list = mains[j].getElementsByTagName("a");
+        var a_list = seach_here[j].getElementsByTagName("a");
         for (i = 0; i < a_list.length; i++) {
             a = a_list[i];
             if (a) {
@@ -21,7 +21,7 @@ function mySearchfilter() {
         }
 
         //Loop through all table row items
-        var tr_list = mains[j].getElementsByTagName("tr");
+        var tr_list = seach_here[j].getElementsByTagName("tr");
         for (i = 0; i < tr_list.length; i++) {
             tr = tr_list[i];
             if (tr) {
